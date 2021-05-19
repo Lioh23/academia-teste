c34eb13d6ad3b2745d54b8a750d9ebbf3a454dfb
Aurélio Silva <aurelin95@gmail.com>
2021-05-18 21:48:22 -0300

    create migration alunos

---
+++ database/migrations/2021_05_19_004535_create_alunos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlunosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('matricula', 11);
+            $table->string('nome', 70);
+            $table->date('dnascimento');
+            $table->string('sexo', 11);
+            $table->string('bairro', 40);
+            $table->string('cidade', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos');
+    }
+}
